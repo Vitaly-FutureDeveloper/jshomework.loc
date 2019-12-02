087e74454b0aab82d51203c050a79b6d3c5f90f0
Супер программист <zarvoron@yandex.ru>
2019-12-02 23:50:25 +0300

    Домашняя работа из курса по JS - Оживление модальных окон при помощи jQuery

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    $('.btn-on-modal').on('click', function(){
+        $('.overlay').fadeIn(800);
+        $('.modal').slideDown(400);
+    });
+
+    $('.close').on('click', function(){
+        $('.overlay').fadeOut(800);
+        $('.modal').slideUp(400);
+    });
+
+
+    $('.overlay').on('click', function(){
+        $('.overlay').fadeOut(800);
+        $('.modal').slideUp(400);
+    });
+
+
+});
